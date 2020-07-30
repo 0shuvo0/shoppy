4300a09d446e135fd3326f237ad997a20e63249f
0shuvo0 <seriouslolz1@gmail.com>
2020-07-30 08:01:01 +0000

    first deploy

---
+++ precache-manifest.918c191bda3090706d7907ce1e2bdb2c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c005a61e265ca1caf1e3",
+    "url": "/shoppy/css/CartItem.6288b045.css"
+  },
+  {
+    "revision": "5a4ce5a6071f3448ba0b",
+    "url": "/shoppy/css/Product.b4f2e9c7.css"
+  },
+  {
+    "revision": "a0ed4ec96160ea67b238",
+    "url": "/shoppy/css/Rating.3127306a.css"
+  },
+  {
+    "revision": "c69bac3afbf6dbb7ccfa",
+    "url": "/shoppy/css/ThemeSelect.e97d67eb.css"
+  },
+  {
+    "revision": "5b8dcfa4583447f68c7d",
+    "url": "/shoppy/css/TopProducts.6428fdd3.css"
+  },
+  {
+    "revision": "a8b5c7cb1b30f73af764",
+    "url": "/shoppy/css/about.52d963c3.css"
+  },
+  {
+    "revision": "8dbd3d732eb45157625f",
+    "url": "/shoppy/css/app.b8711450.css"
+  },
+  {
+    "revision": "402f539e7c4fc78defe4",
+    "url": "/shoppy/css/error.f012bb08.css"
+  },
+  {
+    "revision": "7ad9b6ff62d5208889dfdd7abf7f185f",
+    "url": "/shoppy/img/login-banner.7ad9b6ff.jpg"
+  },
+  {
+    "revision": "5742517ae8905da22d86c252a81beba7",
+    "url": "/shoppy/index.html"
+  },
+  {
+    "revision": "c005a61e265ca1caf1e3",
+    "url": "/shoppy/js/CartItem.3ecc29fe.js"
+  },
+  {
+    "revision": "5a4ce5a6071f3448ba0b",
+    "url": "/shoppy/js/Product.81c87c5f.js"
+  },
+  {
+    "revision": "a0ed4ec96160ea67b238",
+    "url": "/shoppy/js/Rating.8835da37.js"
+  },
+  {
+    "revision": "419c9fa22f3c31e8299d",
+    "url": "/shoppy/js/RegisterForm.72809fb0.js"
+  },
+  {
+    "revision": "c69bac3afbf6dbb7ccfa",
+    "url": "/shoppy/js/ThemeSelect.70121787.js"
+  },
+  {
+    "revision": "5b8dcfa4583447f68c7d",
+    "url": "/shoppy/js/TopProducts.278c5d8b.js"
+  },
+  {
+    "revision": "a8b5c7cb1b30f73af764",
+    "url": "/shoppy/js/about.d24fcebc.js"
+  },
+  {
+    "revision": "8dbd3d732eb45157625f",
+    "url": "/shoppy/js/app.607f483f.js"
+  },
+  {
+    "revision": "b700012a0cbcb8e804d0",
+    "url": "/shoppy/js/chunk-vendors.7ab53af3.js"
+  },
+  {
+    "revision": "402f539e7c4fc78defe4",
+    "url": "/shoppy/js/error.a4d70d2b.js"
+  },
+  {
+    "revision": "270d4c22ce9628f3b092ddd8f4de98d2",
+    "url": "/shoppy/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/shoppy/robots.txt"
+  }
+]);
